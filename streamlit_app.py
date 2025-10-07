f8e0752f5619879a6805dd2b1f2db501ff6acbd6
dipsaha-TD <dip.saha@triedatum.com>
2025-10-07 17:23:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.write("The name on the order is", name_on_order)
 
 # st.write("You selected:", option)
 
-session = get_active_session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
